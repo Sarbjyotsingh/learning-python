672bd14abe7438b92ec77ab34f2d7fe591781f61
Sarbjyotsngh <chahalsarbjyotsingh@gmail.com>
2019-06-18 06:56:28 +0530

    Arithmatic Operators

---
+++ Data types and Operators/arithmeticOperators.py
@@ -0,0 +1,26 @@
+print(1+2)     # Addition
+print(2-1)     # Subtraction
+print(1*2)     # Multiplication
+print(2/2)     # Divison
+print(3 ** 2)  # Exponentiation
+print(9%2)     # Modulo
+print(7//2)    # Integer Divsion
+
+# My electricity bills for the last three months have been $23, $32
+# and $64. What is the average monthly electricity bill over the three month period?
+# Write an expression that calculates the average of 23, 32 and 64
+# Place the expression in this print statement
+print((23+32+64)/3)
+
+
+# you're going to do some calculations for a tiler. Two parts of a floor need tiling.
+# One part is 9 tiles wide by 7 tiles long, the other is 5 tiles wide by 7 tiles long. Tiles come in packages of 6.
+
+# 1 How many tiles are needed?
+# 2 You buy 17 packages of tiles containing 6 tiles each. How many tiles will be left over?
+
+# Fill this in with an expression that calculates how many tiles are needed.
+print(9*7 + 5*7)
+
+# Fill this in with an expression that calculates how many tiles will be left over.
+print(17*6-(9*7 + 5*7))
