bcaad5ade21a58b88a8b70dd18cc6162dbb936c5
Sarbjyotsngh <chahalsarbjyotsingh@gmail.com>
2019-06-21 19:51:26 +0530

    Integers And Floats

---
+++ Data types and Operators/integersAndFloats.py
@@ -0,0 +1,19 @@
+print(3/4)
+print(16/4)
+print(type(4))
+print(type(3.4))
+
+# After decimal point there is no need to put number or 0 
+print(type(3.))
+
+# operation on int and float will always be float
+print(type(3+4.2))
+
+# Converting Float to int
+print(int(49.7))
+
+# converting int to float
+print(float(3520 + 3239))
+
+# python use approximation in floating point numbers in order to fit it in memeory proof:
+print(0.1 +0.1 +0.1)
